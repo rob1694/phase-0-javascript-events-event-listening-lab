0c99be9b4a9ba7db6d4022bb6bae174681c220e7
rob1694 <robertb1694@gmail.com>
2021-03-25 02:41:12 -0400

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
